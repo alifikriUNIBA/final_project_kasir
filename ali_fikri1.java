37a946e01c65066b7b16c34640c34e83c4702fc6
alifikriUNIBA <122199610+alifikriUNIBA@users.noreply.github.com>
2023-01-08 15:55:52 +0700

    Membuat file ali_fikri1.java
    Ini file project saya kak atas nama ali fikri kelas A..

---
+++ ali_fikri1.java
@@ -0,0 +1,15 @@
+package ali_fikri;
+
+import fikri.Condition;
+import fikri.mahasiswa1;
+import java.io.*;
+
+
+public class ali_fikri1 {
+    public static void main (String[] args) throws IOException{
+        do{
+            Condition.percabangan();
+        }while(mahasiswa1.isRunning);
+        }
+    }
+   
